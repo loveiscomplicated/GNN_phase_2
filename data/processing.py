123b1d683d9248c10154fad4c01856f1a928813f
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-11-07 11:02:43 +0900

    [UPDATE] - get_sampled_initial_data 함수 추가, 샘플로 초기 데이터를 생성하여 빠르게 디버깅할 수 있도록 함

--- data/processing.py
+++ data/processing.py
@@ -37,6 +37,38 @@ def get_initial_data(random_state=42):
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 
+def get_sampled_initial_data(size=100, random_state=42):
+    '''
+    missing_corrected.csv를 불러오고,
+    @@전체는 너무 많으니까 잘 돌아가는지 보기 위해 조금만 추출해서@@
+    데이터 스플릿까지 하는 함수
+    Args:
+        random_state(int, optional): 데이터 스플릿할 때 필요한 랜덤 시드
+    '''
+    CURDIR = os.path.dirname(__file__)
+    DATA_PATH = os.path.join(CURDIR, 'missing_corrected.csv')
+    DATA = pd.read_csv(DATA_PATH)
+    DATA.head()
+
+    # 범주형 변수 더미화 시 train/test 간 불일치를 방지하기 위해
+    # 스플릿 전에 전체 데이터 기준으로 가능한 범주를 정의하고 CategoricalDtype으로 고정
+    for col in DATA.columns:
+        cats = list(DATA[col].unique())
+        DATA[col] = DATA[col].astype(pd.api.types.CategoricalDtype(categories=cats))
+
+    DATA = DATA.iloc[:size]
+    
+    # validation set, test set이 imputation 설계하는 데 들어가면 안 됨, 일종의 사후판단 정보가 들어갈 수 있기 때문
+    y = DATA['REASONb']
+    X = DATA.drop('REASONb', axis=1)
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        X, y, test_size=0.30, random_state=random_state
+    )
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp, y_temp, test_size=0.5, random_state=42
+    )
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
 def get_ad_dis_col(df:pd.DataFrame):
     '''
     admission 시의 컬럼, discharge 시의 컬럼을 나누어 리턴
